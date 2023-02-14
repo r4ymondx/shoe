180fb8697e75f6c3bfefdaa1ccaed74778824c9c
raymond <raeewole@gmail.com>
2023-02-14 09:06:58 +0100

    small work

---
+++ shoe.js
@@ -0,0 +1,16 @@
+ var button = document.getElementById('vidual').addEventListener('click',function(){
+     alert("lol can't order dummy");
+ })
+  function buttonClick(){
+      console.log('button click');
+ }
+  var butt = document.querySelectorAll(".btn-danger");
+  console.log(butt);
+  butt.forEach(function(butt) {
+  butt.addEventListener('click', function(){
+      alert("lol you can't order dummy");
+  })
+
+ });
+
+ 
